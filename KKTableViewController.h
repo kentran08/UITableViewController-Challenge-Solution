84f09bd7c035b0d70155fe673826b79c9a3562be
Ken Tran <kentran08@gmail.com>
2014-04-28 23:06:02 -0700

    solved the challenge

---
+++ KKTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KKTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ken Tran on 2014-04-28.
+//  Copyright (c) 2014 Ken Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKTableViewController : UITableViewController
+
+@end
